83913a952a150baa63a2a7d459101aab8c88ce0e
guohaoyu110 <guohaoyu110@gmail.com>
2020-05-13 13:29:47 -0700

    add chatroom

--- ClientMain.cpp
+++ ClientMain.cpp
@@ -1,9 +1,9 @@
-// 我们将函数定义放在头文件Common.h
+#include "Client.h"
 
-// 除了这个功能函数之外，我们还需要把客户端和服务器端共用的宏定义放在 Common.h中，例如：
-// 服务器地址
-// 服务器端口号
-// 消息缓存大小
-// 服务器端默认的欢迎及退出消息
-
-
+// 客户端主函数
+// 创建客户端对象后启动客户端
+int main(int argc, char *argv[]) {
+    Client client;
+    client.Start();
+    return 0;
+}
